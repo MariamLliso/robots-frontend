1f1f132fff61c9ab8d5f8cdbf242e917eb657fdf
MariamLliso <contacto.mariamlliso@gmail.com>
2022-05-15 20:41:45 +0200

    Changes at App, index, and GlobalStyles to match Robot component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
 import AppContainerStyle from "./AppContainerStyle";
 import Header from "./components/layout/Header/Header";
+import RobotsListPage from "./components/pages/RobotsListPage/RobotsListPage";
+import { loadRobotsThunk } from "./redux/thunks/robotsThunks";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRobotsThunk());
+  }, [dispatch]);
+
   return (
     <AppContainerStyle>
       <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/RobotList" />} />
+        <Route path="/RobotList" element={<RobotsListPage />} />
+      </Routes>
     </AppContainerStyle>
   );
 };
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,19 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import store from "./redux/store/store";
 
 describe("Given an App component", () => {
   describe("When it's invoked", () => {
     test("Then it should render a Header Component with with text 'Robotypedia'", () => {
-      render(<App />);
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      );
 
       const header = screen.getByRole("heading", { name: "Robotypedia" });
 
